2cb97a4d8f1be977a3cb384e1837c704efa1eae0
Jan Zimola <zimolajan798@gmail.com>
2024-09-12 17:18:08 +0200

    fixing build error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,10 +2,10 @@ import dbServer from "@/lib/db_server";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: { req: NextRequest }) {
+export async function GET(req: NextRequest) {
   const cookieStore = cookies();
   const supabase = dbServer();
-  const { searchParams } = new URL(req.req.url);
+  const { searchParams } = new URL(req.url);
   const code = searchParams.get("code");
 
   console.log("Starting callback");
@@ -14,7 +14,7 @@ export async function GET(req: { req: NextRequest }) {
   }
 
   console.log("Callback complete");
-  const newUrl = new URL("/dashboard", req.req.url);
+  const newUrl = new URL("/dashboard", req.url);
   console.log("Redirecting to", newUrl.toString());
   return NextResponse.redirect(newUrl);
 }
