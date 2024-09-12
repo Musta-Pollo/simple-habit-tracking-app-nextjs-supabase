26eed54b144087cb05f3d9c91761d1b65e702c7d
Jan Zimola <zimolajan798@gmail.com>
2024-09-12 18:09:07 +0200

    fixing build issues

--- components/form/form-rep-type-picker.tsx
+++ components/form/form-rep-type-picker.tsx
@@ -1,55 +1,55 @@
-"use client";
+//"use client";
 
-import { Enums } from "@/supabase";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
-interface FormRepTypePickerProps {
-  id: string;
-}
+//import { Enums } from "@/supabase";
+//import {
+//  Select,
+//  SelectContent,
+//  SelectGroup,
+//  SelectItem,
+//  SelectTrigger,
+//  SelectValue,
+//} from "../ui/select";
+//interface FormRepTypePickerProps {
+//  id: string;
+//}
 
-import { useState } from "react";
-import { useFormStatus } from "react-dom";
+//import { useState } from "react";
+//import { useFormStatus } from "react-dom";
 
-export const FormRepTypePicker = ({ id }: FormRepTypePickerProps) => {
-  const { pending } = useFormStatus();
-  const [repeatType, setRepeatType] = useState<Enums<"repeatType">>("times");
-  return (
-    <div className="flex flex-col">
-      <input
-        type="text"
-        id={id}
-        name={id}
-        value={repeatType}
-        className="hidden"
-      />
-      <Select
-        onValueChange={(value) => setRepeatType(value as Enums<"repeatType">)}
-        disabled={pending}
-        value={repeatType}
-      >
-        <SelectTrigger className="">
-          <SelectValue defaultValue="times" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {/* <SelectLabel>Projects</SelectLabel> */}
-            <SelectItem value="times">Times</SelectItem>
-            <SelectItem value="mins">Mins</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </div>
-  );
-};
+//export const FormRepTypePicker = ({ id }: FormRepTypePickerProps) => {
+//  const { pending } = useFormStatus();
+//  const [repeatType, setRepeatType] = useState<Enums<"repeat_type">>("Mins");
+//  return (
+//    <div className="flex flex-col">
+//      <input
+//        type="text"
+//        id={id}
+//        name={id}
+//        value={repeatType}
+//        className="hidden"
+//      />
+//      <Select
+//        onValueChange={(value) => setRepeatType(value as Enums<"repeat_type">)}
+//        disabled={pending}
+//        value={repeatType}
+//      >
+//        <SelectTrigger className="">
+//          <SelectValue defaultValue="times" />
+//        </SelectTrigger>
+//        <SelectContent>
+//          <SelectGroup>
+//            {/* <SelectLabel>Projects</SelectLabel> */}
+//            <SelectItem value="times">Times</SelectItem>
+//            <SelectItem value="mins">Mins</SelectItem>
+//          </SelectGroup>
+//        </SelectContent>
+//      </Select>
+//    </div>
+//  );
+//};
 
-interface ProjectSelectItemProps {
-  color: string;
-  name: string;
-  projectId: string;
-}
+//interface ProjectSelectItemProps {
+//  color: string;
+//  name: string;
+//  projectId: string;
+//}
